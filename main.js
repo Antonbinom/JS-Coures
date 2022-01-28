1871f875d88f41952ee59292abf1beb60b2657cf
Anton Semenikhin <protest_the_hero@mail.ru>
2022-01-28 13:30:51 +0300

    Lesson 2 - Price for site

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-let title;
-let screens;
-let screensPrice;
-let fullPrice;
-let adaptive;
+let title = 'Интернет магазин';
+let screens = 'Простые, Сложные, Интерактивные';
+let screensPrice = 3000;
+let rollback = 15;
+let fullPrice = 99900;
+let adaptive = true;
 
-alert('Привет, это мой новый проект!');
-console.log('Выведу это в консоль!');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screensPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+let screensLow = screens.toLowerCase().split(', ');
+console.log(screensLow);
+
+console.log(fullPrice * (rollback / 100));
