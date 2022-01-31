f2aa7ed4a0c89fe324e1f661838553ec7948d68b
Anton Semenikhin <protest_the_hero@mail.ru>
2022-01-31 11:57:33 +0300

    Complete lesson 3

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-let title = 'Интернет магазин';
-let screens = 'Простые, Сложные, Интерактивные';
-let screensPrice = 3000;
-let rollback = 15;
-let fullPrice = 99900;
-let adaptive = true;
+(function () {
+	'use strict';
+}());
+
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать ?', 'Простые, Сложные, Интерактивные');
+let screensPrice = +prompt('Сколько будет стоить данная работа?', '12000');
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?', 'посадка на CMS, размещение на хостинге');
+let servicePrice1 = +prompt('Сколько это будет стоить?', '5000, 10000');
+let service2 = prompt('Какой дополнительный тип услуги нужен?', 'посадка на CMS, размещение на хостинге');
+let servicePrice2 = +prompt('Сколько это будет стоить?', '5000, 10000');
+let rollback = 10;
+
+let fullPrice = screensPrice + servicePrice1 + servicePrice2;
+console.log(fullPrice);
+
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
+console.log(servicePercentPrice);
+
+switch (true) {
+	case fullPrice >= 30000:
+		console.log('Даем скидку в 10%');
+		break;
+	case fullPrice >= 15000 && fullPrice < 30000:
+		console.log('Даем скидку в 5%');
+		break;
+	case fullPrice >= 0 && fullPrice < 15000:
+		console.log('Скидка не предусмотрена');
+		break;
+	case fullPrice < 0:
+		console.log('Что то пошло не так');
+		break;
+}
 
 console.log(typeof title);
 console.log(typeof fullPrice);
