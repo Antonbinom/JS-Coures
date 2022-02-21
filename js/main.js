f82739304cf7416dd2a8b64e596dd8f8f5909843
Anton Semenikhin <protest_the_hero@mail.ru>
2022-02-21 12:34:03 +0300

    delete comments

--- js/main.js
+++ js/main.js
@@ -32,123 +32,111 @@ let screens = document.querySelectorAll('.screen');
 const appData = {
 	title: '',
 	adaptive: true,
-	screens: [], // массив с экранами
-	servicesPercent: {}, // объект с доп услугами %
-	servicesNumber: {}, // объект с доп услугами цены
-	screenPrice: 0, // стоимость всех экранов
+	screens: [],
+	servicesPercent: {},
+	servicesNumber: {},
+	screenPrice: 0,
 	screenNumber: 0,
-	servicePricesPercent: 0, // стоимость доп услуг %
-	servicePricesNumber: 0, // стоимость доп услуг цена
-	fullPrice: 0, // итоговая стоимость
-	rollback: 0, // процент отката
-	servicePercentPrice: 0, // стоимость с учетом отката
+	servicePricesPercent: 0,
+	servicePricesNumber: 0,
+	fullPrice: 0,
+	rollback: 0,
+	servicePercentPrice: 0,
 	cmsBlock: [],
 	cmsPercent: 0,
 
-	// Инициализируем методы
 	init() {
-		this.addTitle(); // Запуск при загрузке страницы
-		startBtn.addEventListener('click', this.checkAddScreens.bind(this)); // Запуск расчетов при нажатии на кнопку Рассчитать
-		resetBtn.addEventListener('click', this.reset.bind(this)); // Запуск расчетов при нажатии на кнопку Рассчитать
-		btnPlus.addEventListener('click', this.addScreenBlock.bind(this)); // Запуск при нажатии на кнопку +
+		this.addTitle();
+		startBtn.addEventListener('click', this.checkAddScreens.bind(this));
+		resetBtn.addEventListener('click', this.reset.bind(this));
+		btnPlus.addEventListener('click', this.addScreenBlock.bind(this));
 		inputRange.addEventListener('input', this.getRollbackPercent.bind(this));
 		inputCms.addEventListener('change', this.openCms);
 		selectCms.addEventListener('change', this.chooseCms);
 		otherInput.addEventListener('input', this.otherCmsPercent.bind(this));
 	},
 
-	addTitle: () => document.title = title.textContent, // Заголовок страницы равен заголовку h1
+	addTitle: () => document.title = title.textContent,
 
-	// запускаем методы рассчета стоимости верстки
 	start() {
-		this.addScreens(); // экраны
-		this.addServices(); // услуги
-		this.addPrices(); // расчеты
-		this.showResult(); // результаты
-		// this.logger();
+		this.addScreens();
+		this.addServices();
+		this.addPrices();
+		this.showResult();
 		this.disabled();
 		this.changeButtons();
 	},
 
-	// Итого
 	showResult() {
-		total.value = this.screenPrice; // сумма верстки экранов
-		totalCount.value = this.screenNumber; // количество экранов
-		totalCountOther.value = this.servicePricesPercent + this.servicePricesNumber; // сумма доп услуг
-		fullTotalCount.value = this.fullPrice; // сумма верстки + доп
-		totalCountRollback.value = this.servicePercentPrice; // итог с учетом отката
+		total.value = this.screenPrice;
+		totalCount.value = this.screenNumber;
+		totalCountOther.value = this.servicePricesPercent + this.servicePricesNumber;
+		fullTotalCount.value = this.fullPrice;
+		totalCountRollback.value = this.servicePercentPrice;
 	},
-	// Результат проверки полей
+
 	checkAddScreens() {
-		if (this.addScreens() === true) { // если при проверке полей возвращается true
-			this.start(); // запускаем расчеты
+		if (this.addScreens() === true) {
+			this.start();
 		} else {
-			alert('Не заполнены поля'); // иначе алерт
+			alert('Не заполнены поля');
 		}
 	},
 
-	// добавляем в массив элементы с типом, количеством экранов и суммарной стоимостью
+
 	addScreens() {
 		this.screens = [];
 
-		screens = document.querySelectorAll('.screen'); // переопределяем переменную screens
-		screens.forEach((screen, index) => { // перебираем массив с
-			const select = screen.querySelector('select'); // типы экранов
-			const input = screen.querySelector('input'); // количество экранов
-			const selectName = select.options[select.selectedIndex].textContent; // проверка на заполнение полей
-			this.screens.push({ // закидываем в массив элементы и указываем их
-				id: index, // индекс
-				name: selectName, // имя выбранного экрана
-				count: +input.value, // количество экранов
-				price: +select.value * +input.value // произведение цены экрана на количество
+		screens = document.querySelectorAll('.screen');
+		screens.forEach((screen, index) => {
+			const select = screen.querySelector('select');
+			const input = screen.querySelector('input');
+			const selectName = select.options[select.selectedIndex].textContent;
+			this.screens.push({
+				id: index,
+				name: selectName,
+				count: +input.value,
+				price: +select.value * +input.value
 			});
 
 		});
 
-		// проверка полей на заполнение
+
 		if (this.screens.find(item => {
-				return item.price == 0; // если найдется хоть одно значение price равное 0, а это будет значить что одно из полей не заполненно
+				return item.price == 0;
 			})) {
-			return false; // позвращаем false
+			return false;
 		} else {
-			return true; //возвращаем true
+			return true;
 		}
 	},
 
-	// добавляем новый блок с экранами
 	addScreenBlock() {
-		const cloneScreen = screens[0].cloneNode(true); // клонируем оригинал блока
-		screens[screens.length - 1].after(cloneScreen); // вставляем клон после оригинала
-
+		const cloneScreen = screens[0].cloneNode(true);
+		screens[screens.length - 1].after(cloneScreen);
 	},
 
-	// Добавляем дополнительные услуги
 	addServices() {
-		// перебираем коллекцию импутов с процентами
 		otherItemsPercent.forEach(item => {
 			const check = item.querySelector('input[type="checkbox"]');
 			const input = item.querySelector('input[type="text"]');
 			const label = item.querySelector('label');
 
-			if (check.checked) { // проверка чекбокса
-				this.servicesPercent[label.textContent] = +input.value; // передаем в объект servicesPercent значение инпутов  otherItemsPercent
+			if (check.checked) {
+				this.servicesPercent[label.textContent] = +input.value;
 			}
 		});
 
-		// перебираем коллекцию импутов с ценой/числами
 		otherItemsNumber.forEach(item => {
 			const check = item.querySelector('input[type="checkbox"]');
 			const input = item.querySelector('input[type="text"]');
 			const label = item.querySelector('label');
-			if (check.checked) { // проверка чекбокса
-				this.servicesNumber[label.textContent] = +input.value; //передаем в объект servicesNumber значение инпутов  otherItemsNumber
+			if (check.checked) {
+				this.servicesNumber[label.textContent] = +input.value;
 			}
 		});
 	},
 
-	// Делаем расчеты
-
-	// Показываем/скрываем select с CMS
 	openCms() {
 		otherBlock.style.display = "none";
 		selectCms.selectedIndex = 0;
@@ -174,37 +162,33 @@ const appData = {
 	otherCmsPercent() {
 		this.cmsPercent = +otherInput.value;
 	},
-	// перебираем элементы в массиве с экранами
+
 	addPrices() {
 		for (let screen of this.screens) {
-			this.screenPrice += +screen.price; // суммируем стоимость всех типов экранов
+			this.screenPrice += +screen.price;
 		}
-		// перебираем элементы в массиве с экранами
 		for (let screen of this.screens) {
-			this.screenNumber += +screen.count; // суммируем количество всех экранов
+			this.screenNumber += +screen.count;
 		}
 
-		// перебираем доп услуги с ценой
 		for (let key in this.servicesNumber) {
-			this.servicePricesNumber += this.servicesNumber[key]; // стоимость доп услуг с ценой
+			this.servicePricesNumber += this.servicesNumber[key];
 		}
 
-		// перебираем доп услуги с процентами
 		for (let key in this.servicePercent) {
-			this.servicePricesPercent += this.screenPrice * (this.servicesPercent[key] / 100); // стоимость доп услуг %
+			this.servicePricesPercent += this.screenPrice * (this.servicesPercent[key] / 100);
 		}
 
-		this.fullPrice = (+this.screenPrice + this.servicePricesNumber + this.servicePricesNumber) // итоговая стоимость
+		this.fullPrice = (+this.screenPrice + this.servicePricesNumber + this.servicePricesNumber);
 		this.fullPrice += this.fullPrice * (this.cmsPercent / 100);
-		this.servicePercentPrice = Math.ceil(this.fullPrice - this.fullPrice * (this.rollback / 100)); // стоимость с учетом отката
+		this.servicePercentPrice = Math.ceil(this.fullPrice - this.fullPrice * (this.rollback / 100));
 	},
 
-	// Значение бегунка заноситься в свойство rollback
 	getRollbackPercent() {
 		inputRangeValue.textContent = inputRange.value + "%";
 		this.rollback = +inputRange.value;
 
-		totalCountRollback.value = Math.ceil(fullTotalCount.value - fullTotalCount.value * (this.rollback / 100)); // изменение стоимости с учетом отката при изменении процента
+		totalCountRollback.value = Math.ceil(fullTotalCount.value - fullTotalCount.value * (this.rollback / 100));
 	},
 
 	disabled() {
